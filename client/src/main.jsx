b38c86c805bda73f955ad69734b8458ca7defbb4
Krushna Chandra Nanda <139350558+Krushna-Nanda@users.noreply.github.com>
2026-03-09 02:40:38 +0530

    adding dark and white switch

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,7 @@ import {BrowserRouter} from 'react-router-dom'
 import { ClerkProvider } from '@clerk/clerk-react'
 import { Provider } from 'react-redux'
 import { store } from './app/store.js'
+import { ThemeProvider } from './context/ThemeContext'
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
@@ -17,7 +18,9 @@ createRoot(document.getElementById('root')).render(
   <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
     <BrowserRouter>
      <Provider store={store}>
+      <ThemeProvider>
         <App />
+      </ThemeProvider>
      </Provider>  
     </BrowserRouter>    
   </ClerkProvider>
